92a01e9707aa127b9e7f23343df4aac111468fbd
Rafael Caputo <rafaelcaputodev@gmail.com>
2021-04-14 21:18:43 -0300

    Correcting date of birth, adding instant created and updated

--- src/main/java/com/caputo/finalWork/entities/Client.java
+++ src/main/java/com/caputo/finalWork/entities/Client.java
@@ -17,11 +17,14 @@ public class Client implements Serializable {
     private String name;
     private String cpf;
     private Double income;
+    private Instant birthDate;
+    private Integer children;
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    private Instant birthDate;
+    private Instant createdAt;
 
-    private Integer children;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
 
     public Client() {
     }
@@ -81,6 +84,24 @@ public class Client implements Serializable {
 
     public void setChildren(Integer children) {
         this.children = children;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = Instant.now();
     }
 
     @Override
